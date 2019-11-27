5515b124ed3ea53c80e040005e2f9b32f5af6179
vladimir <vladimir.smetankin@similarbid.com>
2019-11-27 10:33:39 +0200

    change key

---
+++ precache-manifest.aaf621d92ef47a0929ae157c6116a8c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b23394228299af7ec73805c542ddf6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e3f2b95a0c9f94fb82a",
+    "url": "/static/css/main.693b56f0.chunk.css"
+  },
+  {
+    "revision": "cd3c117fa9e3e3d85a46",
+    "url": "/static/js/2.a98eaf99.chunk.js"
+  },
+  {
+    "revision": "3e3f2b95a0c9f94fb82a",
+    "url": "/static/js/main.acde5b3c.chunk.js"
+  },
+  {
+    "revision": "23c410d5916d9a632eb6",
+    "url": "/static/js/runtime-main.a7baf7d4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8c35491567f01b8850a3913efcfa32d.js"
+  "/precache-manifest.aaf621d92ef47a0929ae157c6116a8c6.js"
 );
 
 self.addEventListener('message', (event) => {
